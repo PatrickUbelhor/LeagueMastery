41f394e51143bb9144894e665ff64a312af1422c
PatrickUbelhor <pubelhor@gmail.com>
2019-07-02 09:42:57 -0500

    Added 'load' button

--- react/src/index.js
+++ react/src/index.js
@@ -21,21 +21,29 @@ class App extends React.Component {
 			username: "VictoryLeech",
 			level: "150"
 		};
+
+		this.getSummoner = this.getSummoner.bind(this);
 	}
 
-	async getSummoner(name) {
-		const result = await Axios.get("http://localhost:3001/api/summoner/by-name/" + name);
+	async getSummoner() {
+		const result = await Axios.get("http://localhost:3001/api/summoner/by-name/VictoryLeech");
 
-		return result.data;
+		this.setState((state, props) => {
+			return {
+				username: result.data.name,
+				level: result.data.summonerLevel
+			};
+		});
+
+		console.log(result.data);
 	}
 
 	render() {
-		this.getSummoner("VictoryLeech");
-
 		return (
 			<div>
 				<Header/>
 				<SummonerSplash region={this.state.region} username={this.state.username} level={this.state.level}/>
+				<button onClick={this.getSummoner}>Load</button>
 				<Table>
 					<ChampionItem champion="Xerath" level="7" points="375,000"/>
 					<ChampionItem champion="Jhin" level="5" points="72,000"/>
