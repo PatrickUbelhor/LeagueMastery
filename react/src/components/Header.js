592b3c1950e886482143c541068a1d21a6926f6b
PatrickUbelhor <pubelhor@gmail.com>
2019-07-29 17:48:36 -0500

    Started transitioning to Material theme

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -1,5 +1,14 @@
 import '../css/Header.css';
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import Paper from '@material-ui/core/Paper';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
 class Header extends React.Component {
 
@@ -17,18 +26,34 @@ class Header extends React.Component {
 	};
 
 	render() {
+		const searchbar = (
+			<Paper className="searchRoot">
+				<IconButton className="searchIconButton" aria-label="menu">
+					<MenuIcon/>
+				</IconButton>
+				<Divider className="searchDivider" />
+				<InputBase
+					className="searchInput"
+					placeholder="Search..."
+					value={this.state.term}
+					onChange={(event) => this.setState({ term: event.target.value })}
+					inputProps={{ 'aria-label': 'Search' }}
+				/>
+				<IconButton className="searchIconButton" aria-label="search">
+					<SearchIcon/>
+				</IconButton>
+			</Paper>
+		);
+
 		return (
-			<nav className='container'>
-				<span className="title">League Mastery</span>
-				<div className="searchDiv">
-					<form onSubmit={this.onFormSubmit}>
-						<input className="searchBox"
-						       value={this.state.term}
-						       onChange={(event) => this.setState({ term: event.target.value })}
-						/>
-					</form>
-				</div>
-			</nav>
+			<AppBar id="appBar" position="static">
+				<Toolbar>
+					<Typography variant="h6" color="inherit">League Mastery</Typography>
+					{/*<form onSubmit={this.onFormSubmit}>*/}
+					{/*</form>*/}
+					{searchbar}
+				</Toolbar>
+			</AppBar>
 		);
 	}
 
