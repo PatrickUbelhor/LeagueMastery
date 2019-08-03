8768fce403c6eec9d3288c92db00a8e594781833
PatrickUbelhor <pubelhor@gmail.com>
2019-08-02 20:41:35 -0500

    Changed region indicator to a Chip component

--- react/src/components/SummonerSplash.js
+++ react/src/components/SummonerSplash.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
 
 function SummonerSplash(props) {
 
@@ -13,7 +14,7 @@ function SummonerSplash(props) {
 				<div className="side-info">
 					<div className="row">
 						<Typography className="name">{props.summoner.name}</Typography>
-						<Typography className="region">{props.region}</Typography>
+						<Chip className="region" color="primary" label={props.region} />
 					</div>
 					<Typography className="level">{"Level " + props.summoner.summonerLevel}</Typography>
 				</div>
