8cbe23f8b309e58a2c981d729ae766d2b4b16044
PatrickUbelhor <pubelhor@gmail.com>
2019-08-02 20:23:49 -0500

    Fixed disclaimer text to properly center

--- react/src/components/Homepage.js
+++ react/src/components/Homepage.js
@@ -7,7 +7,7 @@ function Homepage() {
 		<div>
 			<Typography id="text" variant="h4">Search for a summoner</Typography>
 			<div id="disclaimer">
-				<Typography variant="caption">
+				<Typography id="disclaimerText" variant="caption">
 					League Mastery isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of
 					Riot Games or anyone officially involved in producing or managing League of Legends.
 					League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc.
