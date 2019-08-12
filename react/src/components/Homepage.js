a476ba64b165c80fe004027c6fd5aabccf2a3a8c
PatrickUbelhor <pubelhor@gmail.com>
2019-08-12 18:01:01 -0500

    Changed disclaimer font to grey

--- react/src/components/Homepage.js
+++ react/src/components/Homepage.js
@@ -7,7 +7,7 @@ function Homepage() {
 		<div>
 			<Typography id="text" variant="h4">Search for a Summoner</Typography>
 			<div id="disclaimer">
-				<Typography id="disclaimerText" variant="caption">
+				<Typography id="disclaimerText" variant="caption" color="textSecondary">
 					League Mastery isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of
 					Riot Games or anyone officially involved in producing or managing League of Legends.
 					League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc.
